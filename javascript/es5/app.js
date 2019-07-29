c399f87dbd86d3f8bc4c39bf21812a245666711e
synergy2411 <synergy2411@outlook.com>
2019-07-29 12:48:13 +0530

    js added

---
+++ javascript/es5/app.js
@@ -0,0 +1,37 @@
+// var x = "20";
+
+// console.log(typeof x);
+
+// x = true;
+
+// console.log(typeof x);
+
+
+// var y = 10;
+// var z = 20;
+
+// console.log(x===z);      // ?
+
+// console.log(x + y);     //2010
+// console.log(x - y);     //10
+
+
+
+// // node app.js\
+
+// Non Blocking
+
+// function longRunningOperation(){
+//     setTimeout(function(){
+//         console.log("Running the operation!");
+//     }, 3000);
+// }
+
+// function webRequest(req){
+//     console.log("Starting with ID #", req.id);
+//     longRunningOperation();
+//     console.log("Ending with ID #", req.id);
+// }
+
+// webRequest({id : 1})
+// webRequest({id : 2})
---
+++ javascript/es5/functions.js
@@ -0,0 +1,33 @@
+// var addition = function (num1, num2){
+//     return num1 + num2;
+// }
+
+// // console.log(add(3,5));
+
+// console.log(addition(2,3));
+// console.log(addition);
+
+
+// FUNTION NESTING
+
+// function mystery(){
+//     var chooseNumber = function (){
+//         return 7;
+//     }
+//     return chooseNumber;
+//     var chooseNumber = function (){
+//         return 12;
+//     }
+// }
+
+
+// // console.log(mystery());         //function body
+
+// var nestedFunc = mystery();
+// console.log(nestedFunc());      //?
+
+// IIFE : 
+
+(function(){
+    console.log("IIFE");
+})();
